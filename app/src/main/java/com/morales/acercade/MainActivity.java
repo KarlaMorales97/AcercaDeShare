3d03679c52cc73beffabb60cc339123deab8ee24
AcercaDe <00022516@uca.edu.sv>
2018-04-06 18:18:50 -0600

    Shareeeee

--- app/src/main/java/com/morales/acercade/MainActivity.java
+++ app/src/main/java/com/morales/acercade/MainActivity.java
@@ -1,45 +1,68 @@
 package com.morales.acercade;
 
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button share;
-    String ACTION_SENDTO;
+     ImageView idForSaveView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idForSaveView=(ImageView) findViewById(R.id.Karla);
+    }
+    public void Share(View view){
 
-        share = (Button)findViewById(R.id.boton_share);
+        Bitmap bitmap =getBitmapFromView(idForSaveView);
+        try {
+            File file = new File(this.getExternalCacheDir(),"logicchip.png");
+            FileOutputStream fOut = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+            file.setReadable(true, false);
+            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            intent.putExtra(Intent.EXTRA_TEXT, "Acerca de mi\nNombre: "+getResources().getString(R.string.Nombre)+"\nCarrera: "+getResources().getString(R.string.carrera)
+                    +"\nGitHub: " + getResources().getString(R.string.github)+"\nFacebook: "+getResources().getString(R.string.facebook)
+                    +"\nCorreo: "+ getResources().getString(R.string.correouca)+"\nCelular: "+getResources().getString(R.string.telefono));
+            intent.setType("*/*");
+            startActivity(Intent.createChooser(intent, "Enviar a"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, ((TextView)findViewById(R.id.cajita1)).getText().toString());
-                sendIntent.putExtra(Intent.EXTRA_TEXT, ((TextView)findViewById(R.id.cajita2)).getText().toString());
-                sendIntent.putExtra(Intent.EXTRA_TEXT, ((TextView)findViewById(R.id.cajita3)).getText().toString());
-                sendIntent.putExtra(Intent.EXTRA_TEXT, ((TextView)findViewById(R.id.cajita4)).getText().toString());
-                sendIntent.putExtra(Intent.EXTRA_TEXT, ((TextView)findViewById(R.id.cajita5)).getText().toString());
-                sendIntent.putExtra(Intent.EXTRA_TEXT, ((TextView)findViewById(R.id.cajita6)).getText().toString());
-                sendIntent.setType("text/*");
-                startActivity(sendIntent);
-
-
-                if(sendIntent.resolveActivity(getPackageManager()) != null){
-                    startActivity(sendIntent);
-                }
-            }
-        });
+    private Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable =view.getBackground();
+        if (bgDrawable!=null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas);
+        }   else{
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE);
+        }
+        view.draw(canvas);
+        return returnedBitmap;
     }
 }
